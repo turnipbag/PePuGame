65ff83ec375d463a961e04391bf2ce90fe94c7fc
Tapani Lehtonen <tapani.gabriel.lehtonen@gmail.com>
2022-10-25 22:00:06 +0300

    Broken player positioning.

--- turtle_racing.py
+++ turtle_racing.py
@@ -1,4 +1,9 @@
-import pygame, sys
+import pygame, sys, random
+
+player_list = ["Tapani", "Mikko"]
+color_list = ["red", "green"]
+
+player_info = list(zip(player_list, color_list))
 
 clock = pygame.time.Clock()
 
@@ -8,15 +13,28 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 
 pygame.display.set_caption("Test game")
 
-turtle_surf = pygame.Surface((20,20))
-turtle_rect = turtle_surf.get_rect(center = (10,360))
+players= []
+texts = []
+spot = 1
+for player in player_info:
+    name = player[0]
+    color = player[1]
+
+    turtle_surf = pygame.Surface((20,20))
+    turtle_rect = turtle_surf.get_rect(center = (20, 20 + 40 * spot))
+
+    font = pygame.font.SysFont("Times New Roman", 15)
+    text_surf = font.render(name,False,color)
+    text_rect = text_surf.get_rect(center = (25, 10 + 30 * spot))
+
+    spot += 1
+
+    players.append((turtle_surf,turtle_rect))
+    texts.append((text_surf,text_rect))
 
 finish_surf = pygame.Surface((40,720))
 finish_rect = finish_surf.get_rect(center = (1200, 360))
 
-font = pygame.font.SysFont("Times New Roman", 15)
-text_surf = font.render("TAPANI", False,(232, 193, 86))
-text_rect = text_surf.get_rect(center = (10,340))
 
 speed = 0
 
@@ -26,20 +44,23 @@ while True:
             pygame.quit()
             sys.exit()
 
-
-    turtle_surf.fill((232, 193, 86))
     finish_surf.fill(("white"))
 
     display_surface.fill("black")
     display_surface.blit(finish_surf, finish_rect)
-    display_surface.blit(turtle_surf, turtle_rect)
-    display_surface.blit(text_surf, text_rect)
 
-    
+    #draw each player separately
+    for player, color in zip(players,color_list):
+        player[0].fill(color)
+        display_surface.blit(player[0], player[1])
+    for text in texts:
+        display_surface.blit(text[0], text[1])
+
+    """
     if not turtle_rect.colliderect(finish_rect):
         turtle_rect.x += 5
         text_rect.x += 5
-
+    """
     clock.tick(60)
 
 
